ab08debc5d963b5ba90e67a68342dc8e1e6a8611
Dave Shanley <dave@quobix.com>
2022-07-08 10:09:24 -0400

    updated to 0.0.9

--- Formula/vacuum.rb
+++ Formula/vacuum.rb
@@ -1,26 +1,26 @@
 class Vacuum < Formula
     desc "vacuum"
     homepage "https://quobix.com/vacuum"
-    version "0.0.8"
+    version "0.0.9"
   
     if OS.mac?
-      url "https://github.com/daveshanley/vacuum/releases/download/v0.0.8/vacuum_0.0.8_Darwin_x86_64.tar.gz"
-      sha256 "df8beca2bb60ecc995aee8b82127b989363237a7b7fb3c04eabc82a2b7923a4d"
+      url "https://github.com/daveshanley/vacuum/releases/download/v0.0.9/vacuum_0.0.9_Darwin_x86_64.tar.gz"
+      sha256 "5435239474edc859d86cda4760320cc7013ec177b9e85fe497d84409f641e631"
     end
   
     if OS.mac? && Hardware::CPU.arm?
-        url "https://github.com/daveshanley/vacuum/releases/download/v0.0.8/vacuum_0.0.8_Darwin_arm64.tar.gz"
-        sha256 "cb50e844a3174140eaae42a368a3a50b37e77822d790ffa555a686ad3712f98b"
+        url "https://github.com/daveshanley/vacuum/releases/download/v0.0.9/vacuum_0.0.9_Darwin_arm64.tar.gz"
+        sha256 "5c5692e5d69fbfff1252b57616146409c9f59059135d4426f4a030d01da92d8a"
     end
   
     if OS.linux? && Hardware::CPU.intel?
-      url "https://github.com/daveshanley/vacuum/releases/download/v0.0.8/vacuum_0.0.8_Linux_x86_64.tar.gz"
-      sha256 "d4aa58da08bfc5fe926c2571f552266842a33aa3799094f671e8ac612987fb77"
+      url "https://github.com/daveshanley/vacuum/releases/download/v0.0.9/vacuum_0.0.9_Linux_x86_64.tar.gz"
+      sha256 "9733656d695826a0f65862fcf06c0d139a3d8f41b964a16b9dfe386d91a08b57"
     end
   
     if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/daveshanley/vacuum/releases/download/v0.0.8/vacuum_0.0.8_Linux_arm64.tar.gz"
-      sha256 "40a31c870b8c3daf815d1a99689d5e393714722674ed07a24dff4def412197fe"
+      url "https://github.com/daveshanley/vacuum/releases/download/v0.0.9/vacuum_0.0.9_Linux_arm64.tar.gz"
+      sha256 "3b094da185a799c65ab67f7982dc402092164a11b476d2bb75bb185a67081658"
     end
   
     def install
